5b5517f28b94813f83b6e17f2ec8304545d011dc
henan.zhou <henan.zhou@tusimple.ai>
2024-02-19 17:19:11 +0800

    add build.sh

---
+++ problems/two_sum/two_sum.cpp
@@ -0,0 +1,17 @@
+#include "headers.h"
+
+class Solution {
+public:
+
+};
+TEST(two_sum, two_sum)
+{
+    Solution s;
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
